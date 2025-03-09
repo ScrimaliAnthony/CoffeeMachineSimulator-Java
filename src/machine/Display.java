d134fbc17d55d90d289366e441bd698876020835
Scrimali Anthony <anthony.scrimali@gmail.com>
2025-03-09 09:11:45 +0100

    [feat] : Ajout du menu

---
+++ src/machine/Display.java
@@ -0,0 +1,15 @@
+package machine;
+
+public class Display {
+
+    public static String menu() {
+
+        return "Starting to make a coffee" + System.lineSeparator() +
+                "Grinding coffee beans" + System.lineSeparator() +
+                "Boiling water" + System.lineSeparator() +
+                "Mixing boiled water with crushed coffee beans" + System.lineSeparator() +
+                "Pouring coffee into the cup" + System.lineSeparator() +
+                "Pouring some milk into the cup" + System.lineSeparator() +
+                "Coffee is ready!";
+    }
+}
